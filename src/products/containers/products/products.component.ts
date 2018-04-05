08fa74f97d5f13422e65f7b6c1ce26c17962a49f
Markus Sjöholm <markus@slingingcode.net>
2018-04-05 14:16:58 +0100

    Effects and Entities 2 of 3. Done with Our first @Effect

--- src/products/containers/products/products.component.ts
+++ src/products/containers/products/products.component.ts
@@ -38,10 +38,7 @@ export class ProductsComponent implements OnInit {
   constructor(private store: Store<fromStore.ProductsState>) {}
 
   ngOnInit() {
-    // this.store.select(fromStore.getAllPizzas).subscribe(state => {
-    //   console.log(state);
-    // })
     this.pizzas$ = this.store.select(fromStore.getAllPizzas);
-
+    this.store.dispatch(new fromStore.LoadPizzas());
   }
 }
