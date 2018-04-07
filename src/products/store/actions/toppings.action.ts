1c26d21de3323ce86df92b9e24d2d1102df7478e
Markus Sjöholm <markus@slingingcode.net>
2018-04-07 20:32:02 +0100

    Extending our State Tree - Mapping IDs to Entities

--- src/products/store/actions/toppings.action.ts
+++ src/products/store/actions/toppings.action.ts
@@ -6,6 +6,8 @@ export const LOAD_TOPPINGS = '[Products] Load Toppings';
 export const LOAD_TOPPINGS_FAIL = '[Products] Load Toppings Fail';
 export const LOAD_TOPPINGS_SUCCESS = '[Products] Load Toppings Success';
 
+export const VISUALISE_TOPPINGS = '[Products] Visualise Toppings';
+
 export class LoadToppings implements Action {
   readonly type = LOAD_TOPPINGS;
 }
@@ -20,5 +22,10 @@ export class LoadToppingsSuccess implements Action {
   constructor(public payload: Topping[]) {}
 }
 
+export class VisualizeToppings implements Action {
+  readonly type = VISUALISE_TOPPINGS;
+  constructor(public payload: number[]) {};
+}
+
 // action types
-export type ToppingsAction = LoadToppings | LoadToppingsFail | LoadToppingsSuccess;
+export type ToppingsAction = LoadToppings | LoadToppingsFail | LoadToppingsSuccess | VisualizeToppings;
--- src/products/store/reducers/toppings.reducer.ts
+++ src/products/store/reducers/toppings.reducer.ts
@@ -5,16 +5,27 @@ export interface ToppingsState {
   entities: { [id: number]: Topping };
   loaded: boolean;
   loading: boolean;
+  selectedToppings: number[];
 }
 
 export const initialState: ToppingsState = {
   entities: {},
   loaded: false,
-  loading: false
+  loading: false,
+  selectedToppings: []
 };
 
 export function reducer(state = initialState, action: fromToppings.ToppingsAction): ToppingsState {
   switch(action.type) {
+
+    case fromToppings.VISUALISE_TOPPINGS: {
+      const selectedToppings = action.payload;
+      return {
+        ...state,
+        selectedToppings
+      }
+    }
+
     case fromToppings.LOAD_TOPPINGS: {
       return {
         ...state,
@@ -53,6 +64,7 @@ export function reducer(state = initialState, action: fromToppings.ToppingsActio
         loading: false
       }
     }
+
   }
 
   return state;
@@ -61,3 +73,4 @@ export function reducer(state = initialState, action: fromToppings.ToppingsActio
 export const getToppingEntities = (state: ToppingsState) => state.entities;
 export const getToppingsLoaded = (state: ToppingsState) => state.loaded;
 export const getToppingsLoading = (state: ToppingsState) => state.loading;
+export const getSelectedToppings = (state: ToppingsState) => state.selectedToppings;
